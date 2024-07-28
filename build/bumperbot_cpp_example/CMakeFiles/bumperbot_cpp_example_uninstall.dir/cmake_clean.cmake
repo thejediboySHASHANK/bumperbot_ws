3433df5a83faf6a30cd395b234a46a68c5bd2e56
thejediboySHASHANK <21052358@kiit.ac.in>
2024-07-28 15:19:53 +0530

    Added example publisher node in Python, modified setup.py file to include config and added exec_dependencies in xml

---
+++ build/bumperbot_cpp_example/CMakeFiles/bumperbot_cpp_example_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bumperbot_cpp_example_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/bumperbot_cpp_example_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/bumperbot_py_examples/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/shashank/bumperbot_ws/install/bumperbot_py_examples'
